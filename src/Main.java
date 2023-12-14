f5428518cd02f70b7a364956c7026bfcf955021b
ugur <uysallugur@outlook.com>
2023-12-14 22:08:53 +0300

    Kullanıcıdan alınan tutara göre kdv tutarı hesaplayan program

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18, kdvliTutar, kdvTutar;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Tutar giriniz : ");
+        tutar = inp.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz Tutar : " + tutar);
+        System.out.println("KDV Oranı : " + kdvOran);
+        System.out.println("KDV Tutarı : " + kdvTutar);
+        System.out.println("KDV'li Tutarı : " + kdvliTutar);
+    }
+}
